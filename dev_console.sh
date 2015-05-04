a430f8523407feb2274639c842e5135eaa3d64fb
Jack Tang <jack@taodinet.com>
2015-05-04 18:15:11 +0800

    Improve project templates

--- dev_console.sh
+++ dev_console.sh
@@ -1,7 +1,7 @@
 erl -pa ./ebin/ -pa deps/*/ebin \
     {{ name_option }} \
     {{ erl_vm_flags }} \
-    {{ env_options }} \
+    {{ env_vars }} \
     {{ config_files }} \
     {{ mnesia_options }} \
     {{ start_services }} \
